ff6cdeb64cd10d59276e66ffe1c1db39cb1ec313
manasmanohar <21006907+manasmanohar@users.noreply.github.com>
2023-03-25 21:55:22 +0530

    modified layout for landingpage

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import NavBar from '@/components/NavBar'
 import Header from '@/components/Header'
-const Layout = ({ children }) => {
+
+const Layout = ({ children, includeLayout }) => {
     return (
-        <div className="flex flex-col lg:flex-row-reverse w-full h-screen  bg-white">
-            <Header className="sticky top-0" />
-            <main className="flex-1 lg:w-3/4  ">{children}</main>
-            <NavBar className="lg:w-1/4 flex-shrink-0" />
+        <div className="flex flex-col lg:flex-row-reverse w-full h-screen bg-white">
+            {includeLayout && <Header className="sticky top-0" />}
+            <main className="flex-1 lg:w-3/4">{children}</main>
+            {includeLayout && <NavBar className="lg:w-1/4 flex-shrink-0" />}
         </div>
     )
 }
